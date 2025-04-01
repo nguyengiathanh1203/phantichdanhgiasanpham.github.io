96d213667dc4f4aa88a1063d623049b096021d26
ngquangduong <116612879+ngquangduong@users.noreply.github.com>
2025-04-01 03:20:31 +0000

    Update Học Machine Learning

--- phantichdanhgiasanpham/Học Machine Learning bằng Python/test.py
+++ phantichdanhgiasanpham/Học Machine Learning bằng Python/test.py
@@ -1,4 +1,17 @@
-i = 0
-while i < 10:
-    print (i)
-    i += 1
+# dataframe
+import numpy as np
+import pandas as pd
+
+myarray = np.array([[1, 2, 3], [4, 5, 6]])
+rownames = ['a', 'b']
+colnames = ['one', 'two', 'three']
+
+mydataframe = pd.DataFrame(myarray, index=rownames, columns=colnames)
+
+print(mydataframe)
+
+print("method 1:")
+print("one column: %s" % mydataframe['one'])
+
+print("method 2:")
+print("one column: %s" % mydataframe.one)
